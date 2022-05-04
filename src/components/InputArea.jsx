96c11b5a9f992174ee7342fc8a258f210d5e2853
abdelrahmanhassan1 <abdelrahmanwork1@gmail.com>
2022-05-04 22:41:04 +0200

    adding decrypt functions

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -30,16 +30,18 @@ const letters = [
 ];
 
 var encoded_msg = "";
+var decoded_msg = "";
 
+// main function
 function InputArea() {
-  const [msg, setMsg] = useState({
+  const [encryptMsg, setEncryptMsg] = useState({
     message: "",
     shift: "",
   });
 
-  function handleChange(event) {
+  function handleEncryptChange(event) {
     const { name, value } = event.target;
-    setMsg((prevValue) => {
+    setEncryptMsg((prevValue) => {
       return {
         ...prevValue,
         [name]: value,
@@ -58,7 +60,15 @@ function InputArea() {
         while (new_position > letters.length - 1) {
           new_position -= letters.length;
         }
-        new_message += letters[new_position];
+        // want to make the upper and lower characters as inserted
+        // here the letter inserted is in lower case
+        if (message.charAt(i) === message.charAt(i).toLowerCase()) {
+          new_message += letters[new_position];
+        }
+        // other wise the letter is capital
+        else {
+          new_message += letters[new_position].toUpperCase();
+        }
       } else {
         new_message += letter;
       }
@@ -66,10 +76,67 @@ function InputArea() {
     return new_message;
   }
 
-  function handleSubmit(event) {
-    if (msg.shift !== "" && msg.message !== "") {
-      encoded_msg = encryptMessage(msg.shift, msg.message);
-      setMsg({
+  function handleEncryptSubmit(event) {
+    if (encryptMsg.shift !== "" && encryptMsg.message !== "") {
+      encoded_msg = encryptMessage(encryptMsg.shift, encryptMsg.message);
+      setEncryptMsg({
+        message: "",
+        shift: "",
+      });
+    } else {
+      alert("Must Enter shift amount and Message");
+    }
+    event.preventDefault();
+  }
+
+  // Decrypt Methods
+  const [decryptMsg, setDecryptMsg] = useState({
+    message: "",
+    shift: "",
+  });
+
+  function handleDecryptChange(event) {
+    const { name, value } = event.target;
+    setDecryptMsg((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+
+  function DecryptMessage(shift, message) {
+    shift = parseInt(shift);
+    var new_message = "";
+    for (let i = 0; i < message.length; i++) {
+      let letter = message.charAt(i).toLowerCase();
+      if (letters.includes(letter)) {
+        let current_position = letters.indexOf(letter);
+        let new_position = current_position - shift;
+        while (new_position < 0) {
+          new_position += letters.length;
+        }
+        // want to make the upper and lower characters as inserted
+        // here the letter inserted is in lower case
+        if (message.charAt(i) === message.charAt(i).toLowerCase()) {
+          new_message += letters[new_position];
+        }
+        // other wise the letter is capital
+        else {
+          new_message += letters[new_position].toUpperCase();
+        }
+      } else {
+        new_message += letter;
+      }
+    }
+    return new_message;
+  }
+
+  function handleDecryptSubmit(event) {
+    if (decryptMsg.shift !== "" && decryptMsg.message !== "") {
+      decoded_msg = DecryptMessage(decryptMsg.shift, decryptMsg.message);
+
+      setDecryptMsg({
         message: "",
         shift: "",
       });
@@ -80,28 +147,54 @@ function InputArea() {
   }
 
   return (
-    <div>
+    <div className="container">
+      {/* encrypt container  */}
       <form className="encrypt-container">
         <h3>Enter Message to be Encrypted</h3>
         <textarea
           name="message"
           rows={3}
           placeholder={"Hello World!"}
-          onChange={handleChange}
-          value={msg.message}
+          onChange={handleEncryptChange}
+          value={encryptMsg.message}
         ></textarea>
         <h3>Enter shift</h3>
         <input
           name="shift"
           placeholder={"4"}
-          onChange={handleChange}
+          onChange={handleEncryptChange}
           type="number"
-          value={msg.shift}
+          value={encryptMsg.shift}
         ></input>
 
-        <button onClick={handleSubmit}>Encrypt</button>
+        <button onClick={handleEncryptSubmit}>Encrypt</button>
 
-        <h1>Encoded Message:{encoded_msg}</h1>
+        <h1>Encoded Message</h1>
+        <h2>{encoded_msg}</h2>
+      </form>
+      {/* decrypt container  */}
+      <form className="decrypt-container">
+        <h3>Enter Message to be Decrypted</h3>
+        <textarea
+          name="message"
+          rows={3}
+          placeholder={"Lipps Asvph!"}
+          onChange={handleDecryptChange}
+          value={decryptMsg.message}
+        ></textarea>
+        <h3>Enter shift</h3>
+        <input
+          name="shift"
+          placeholder={"4"}
+          onChange={handleDecryptChange}
+          type="number"
+          value={decryptMsg.shift}
+        ></input>
+
+        <button onClick={handleDecryptSubmit}>Decrypt</button>
+
+        <h1>Decoded Message</h1>
+        <h2>{decoded_msg}</h2>
       </form>
     </div>
   );
